49059bf146718568c49b53534f60cd865ae47514
Diya Zhao <zdy.gogo@msn.com>
2016-07-15 10:22:09 -0700

    guess box focus,guessd list style

--- js/app.js
+++ js/app.js
@@ -5,51 +5,49 @@ $(document).ready(function(){
 	var originalCount = $("#count").html();
 	var orignalList = $('#guessList').html();
 	var guessedNum =[];
+	var orignalFeedback = $('#feedback').html();
 
 	function newGame(){
 		secretNumber = generateNumber();
 		$("#count").html(originalCount);
 		$('#guessList').html(orignalList);
+		$('#feedback').html(orignalFeedback);
+		userGuess = $('#userGuess').val()
+		guessedNum = [];
 		console.log(secretNumber)
 	}
+
 	newGame()
 
 	function generateNumber(){
 		return Math.floor(Math.random()*100+1);
-
-
 	}
 
 	$('#guessButton').on("click",function(e){
 		e.preventDefault();
 		userGuess = $('#userGuess').val()
+		$('#userGuess').focus()
+		console.log(userGuess)
 		//check if user's input is integer 
 		for(var i=0;i<guessedNum.length;i++){
 			if(guessedNum[i] == userGuess) {
 				alert("You already guessed this number!")
 				userGuess = $('#userGuess').val('')
 				return false;
-
 			}
 		}
-		if(userGuess%1 !== 0){
+		if(userGuess == null || userGuess ==''){
+			alert('Please input an integer');
+		}
+		else if(userGuess%1 !== 0||userGuess>100){
 			alert("Please input an integer between 1 and 100")
 		}
-	
 		else {
 			getFeedback(userGuess);
 			guessList();
 			guessCount();
 		}
-	//function都执行完之后才能，输入空白，不然空白会被当做arguement执行
-	userGuess = $('#userGuess').val('')
-		
-		//get feedback about each guess
-		//give range feedback
-		//appear in h2#feedback
-		//count guesses
-		//list guessed numbers
-
+		userGuess = $('#userGuess').val('')
 	});
 
 	function getFeedback(userGuess){
@@ -68,22 +66,19 @@ $(document).ready(function(){
 		else if((Math.abs(secretNumber - userGuess))>50){
 			$('#feedback').text('Ice cold')
 		} 
-		
 	}
 
 	function guessList(){
 		userGuess = $('#userGuess').val();
-		$('#guessList').append(userGuess+', ');
+		$('#guessList').append('<li>'+userGuess+'</li>');
 		guessedNum.push(userGuess);
 	}
-
 
 	function guessCount(){
 		var num = parseInt($('#count').text());
 		$('#count').text(num+1);
 	}
-
-	$('.new').on("click",newGame);
 	
+	$('.new').on("click",newGame);
 
 })
